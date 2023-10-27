c27ce06b4f2d956b57d1dd3ba4b5f9429453cc80
Arraca <141419613+Arraca@users.noreply.github.com>
2023-10-27 10:07:06 +0200

    Squashed commit of the following:
    commit be4c2698e3c007e90d1801b5c826aa966b7d3067
    Author: Giacomo <giava99@gmail.com>
    Date:   Fri Oct 27 09:57:48 2023 +0200
    
        seasnv3
    
    commit 0322a019b155b135193088c615fa7866ef052f8e
    Author: Giacomo <giava99@gmail.com>
    Date:   Fri Oct 27 09:55:15 2023 +0200
    
        season
    
    commit 9886eebfd8222385b55641eb2996a0e39b3645e8
    Author: Giacomo <giava99@gmail.com>
    Date:   Fri Oct 27 09:51:43 2023 +0200
    
        SEASON COMPPLETA
    
    commit 76a5f73f1e883324215013795af2520b7cffcaac
    Author: Giacomo <giava99@gmail.com>
    Date:   Thu Oct 26 18:08:02 2023 +0200
    
        Con Season
    
    commit 1026306d9c3bf7bfb7e70a54a9e4b899a2d09f5a
    Author: Giacomo <giava99@gmail.com>
    Date:   Thu Oct 26 16:49:46 2023 +0200
    
        provaprovaa

--- src/main/java/com/generation/javabnb/model/repositories/SeasonRepository.java
+++ src/main/java/com/generation/javabnb/model/repositories/SeasonRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.javabnb.model.entities.Season;
 
+<<<<<<< HEAD
 public interface SeasonRepository extends JpaRepository<Season, Integer>
+=======
+public interface SeasonRepository extends JpaRepository<Season,Integer>
+>>>>>>> origin/javabnbGiacomo
 {
 
 }
